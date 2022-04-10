1ac2b73d0bc1100e8dad755dff367672def5ff62
Vitaly Sizikov <606er.54rus@gmail.com>
2022-04-10 18:07:21 +0700

    NEW: Hot reload inside docker-compose for local

--- backend/scripts/run.local.sh
+++ backend/scripts/run.local.sh
@@ -3,4 +3,6 @@
 ./scripts/wait-for-it.sh db:5432 -t 15
 ./scripts/wait-for-it.sh redis:6379 -t 15
 
-dramatiq app.dramatiq:DRAMATIQ_BROKER app.tasks --processes 2 --threads 2
+alembic upgrade head
+
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
--- backend/scripts/run_dramatiq.local.sh
+++ backend/scripts/run_dramatiq.local.sh
@@ -3,4 +3,4 @@
 ./scripts/wait-for-it.sh db:5432 -t 15
 ./scripts/wait-for-it.sh redis:6379 -t 15
 
-dramatiq app.dramatiq:DRAMATIQ_BROKER app.tasks --processes 2 --threads 2
+dramatiq app.dramatiq:DRAMATIQ_BROKER app.tasks --watch app --processes 1 --threads 1
