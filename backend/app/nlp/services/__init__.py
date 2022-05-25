8a931e0dea7ea0a1fe3f1313e98030a220a32f52
Vitaly Sizikov <606er.54rus@gmail.com>
2022-05-25 22:12:08 +0700

    fixed circular import

--- backend/app/nlp/services/__init__.py
+++ backend/app/nlp/services/__init__.py
@@ -1,8 +1,9 @@
 # flake8: noqa
+
+from .lev_provider import LevenshteinProvider
+from .nat_provider import NatashaProvider
+from .finder import FeatureFidnder
 from .anonymizer import Anonymizer
 from .controller import Controller
 from .extractor import FeatureExtractor
-from .finder import FeatureFidnder
-from .lev_provider import LevenshteinProvider
-from .nat_provider import NatashaProvider
 from .speller import Speller
